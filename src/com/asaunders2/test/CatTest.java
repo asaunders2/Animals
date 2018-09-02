0394af6dc00a6026deb0bf10c1215dfcec1a84e7
Your Name <you@example.com>
2018-09-01 19:23:45 -0500

    CatTest finished

--- src/com/asaunders2/test/CatTest.java
+++ src/com/asaunders2/test/CatTest.java
@@ -1,14 +1,20 @@
 package com.asaunders2.test;
 
+import com.asaunders2.Cat;
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CatTest extends TestCase {
+import static org.junit.Assert.assertEquals;
+
+public class CatTest{
+
+    Cat testCat;
 
     @Before
     public void setUp() throws Exception {
+        testCat = new Cat(5, "Tom");
     }
 
     @After
@@ -17,18 +23,11 @@ public class CatTest extends TestCase {
 
     @Test
     public void getMousesKilled() {
+        assertEquals(5, testCat.getMousesKilled());
     }
 
     @Test
     public void addMouse() {
     }
 
-    @Test
-    public void talk() {
-    }
-
-    @Test
-    public String toString() {
-        return null;
-    }
 }
